fe9b4ab41b0119c75e49ecb518cdede3bbbdc814
Rory <srsullivan01@gmail.com>
2017-10-24 10:54:38 -0400

    built framework

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `<h1>{{title}}</h1><h2>{{hero}} details </h2>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Tour of Heroes';
+  hero = 'Windstorm';
 }
